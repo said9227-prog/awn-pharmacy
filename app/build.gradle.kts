85ad4f658b062688077fea81a6e371bf51145683
said9227-prog <said774649431@gmail.com>
2026-06-17 17:25:52 +0300

    Update build.gradle.kts for SDK and config changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,74 +1,64 @@
-plugins {
-alias(libs.plugins.android.application)
-alias(libs.plugins.kotlin.compose)
-alias(libs.plugins.google.devtools.ksp)
-alias(libs.plugins.roborazzi)
-alias(libs.plugins.secrets)
-}
-
 android {
 namespace = "com.example"
 
-compileSdk = 34
+compileSdk = 36
 
 defaultConfig {
-applicationId = "com.aistudio.aonpharma.xjkwp"
-minSdk = 24
-targetSdk = 31
-versionCode = 1
-versionName = "1.0"
+    applicationId = "com.aistudio.aonpharma.qzmrxy"
+    minSdk = 24
+    targetSdk = 31
+    versionCode = 1
+    versionName = "1.0"
 
-testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 }
 
 signingConfigs {
-create("release") {
-val keystorePath = System.getenv("KEYSTORE_PATH") ?: "${rootDir}/my-upload-key.jks"
-storeFile = file(keystorePath)
-storePassword = System.getenv("STORE_PASSWORD")
-keyAlias = "upload"
-keyPassword = System.getenv("KEY_PASSWORD")
-}
+    create("release") {
+        val keystorePath = System.getenv("KEYSTORE_PATH") ?: "${rootDir}/my-upload-key.jks"
+        storeFile = file(keystorePath)
+        storePassword = System.getenv("STORE_PASSWORD")
+        keyAlias = "upload"
+        keyPassword = System.getenv("KEY_PASSWORD")
+    }
 
-create("debugConfig") {
-  storeFile = file("${rootDir}/debug.keystore")
-  storePassword = "android"
-  keyAlias = "androiddebugkey"
-  keyPassword = "android"
-}
-
+    create("debugConfig") {
+        storeFile = file("${rootDir}/debug.keystore")
+        storePassword = "android"
+        keyAlias = "androiddebugkey"
+        keyPassword = "android"
+    }
 }
 
 buildTypes {
-release {
-isCrunchPngs = false
-isMinifyEnabled = false
-proguardFiles(
-getDefaultProguardFile("proguard-android-optimize.txt"),
-"proguard-rules.pro"
-)
-signingConfig = signingConfigs.getByName("release")
-}
+    release {
+        isCrunchPngs = false
+        isMinifyEnabled = false
+        proguardFiles(
+            getDefaultProguardFile("proguard-android-optimize.txt"),
+            "proguard-rules.pro"
+        )
+        signingConfig = signingConfigs.getByName("release")
+    }
 
-debug {
-}
-
+    debug {
+    }
 }
 
 compileOptions {
-sourceCompatibility = JavaVersion.VERSION_11
-targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 buildFeatures {
-compose = true
-buildConfig = true
+    compose = true
+    buildConfig = true
 }
 
 testOptions {
-unitTests {
-isIncludeAndroidResources = true
+    unitTests {
+        isIncludeAndroidResources = true
+    }
 }
-}
+
 }
